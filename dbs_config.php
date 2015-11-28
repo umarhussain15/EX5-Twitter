10049e12edb31abac25f36d9091ac608ff0f4e14
umarhussain15 <umarhussain.work@gmail.com>
2015-11-28 12:33:51 +0500

    twitter app
    simple twitter app with mysql php angularjs

---
+++ dbs_config.php
@@ -0,0 +1,11 @@
+<?php
+ 
+/*
+ * All database connection variables
+ */
+ 
+define('DB_USER', "web"); // db user
+define('DB_PASSWORD', "seecs@123"); // db password (mention your db password here)
+define('DB_DATABASE', "webex5"); // database name
+define('DB_SERVER', "localhost"); // db server
+?>
